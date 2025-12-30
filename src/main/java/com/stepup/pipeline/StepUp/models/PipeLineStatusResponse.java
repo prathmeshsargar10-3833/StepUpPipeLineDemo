2cb48996566b64589f9f9a4c1b4243db6bdd87a0
Prathmesh <prathmeshsargar10@gmail.com>
2025-12-30 10:22:09 +0530

    Initial commit

---
+++ src/main/java/com/stepup/pipeline/StepUp/models/PipeLineStatusResponse.java
@@ -0,0 +1,15 @@
+package com.stepup.pipeline.StepUp.models;
+
+
+import lombok.Data;
+import lombok.Generated;
+
+import java.util.UUID;
+
+@Data
+public class PipeLineStatusResponse {
+
+    private String id= UUID.randomUUID().toString();
+    private String message;
+    private String timestamp = String .valueOf(System.currentTimeMillis());
+}
